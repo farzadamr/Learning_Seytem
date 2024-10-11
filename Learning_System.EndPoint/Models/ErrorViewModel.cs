ee0f8522739f2d34e7a41985aa7038e0df204f2c
Farzad <amirifarzad1381@gmail.com>
2024-10-11 19:24:20 +0330

    add layout page

---
+++ Learning_System.EndPoint/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Learning_System.EndPoint.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

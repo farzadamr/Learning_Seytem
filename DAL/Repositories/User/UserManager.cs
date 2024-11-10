5e6d32b8ad94a5f9b45d5df24b84dd87eb9e7bd6
Farzad <amirifarzad1381@gmail.com>
2024-11-10 13:07:09 +0330

    add usermanager class to auth-users

---
+++ DAL/Repositories/User/UserManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repositories.User
+{
+    public class UserManager
+    {
+        private readonly string _connection;
+        public UserManager(string _connection)
+        {
+            this._connection = _connection;
+        }
+
+    }
+}

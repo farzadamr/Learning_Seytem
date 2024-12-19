5be0351d54303d5bf4dc0e07ad1198fc1ebd826b
Farzad <amirifarzad1381@gmail.com>
2024-12-20 01:26:16 +0330

    add-person service razor

--- AdminPanel.EndPoint/Pages/Users/Person/Create.cshtml.cs
+++ AdminPanel.EndPoint/Pages/Users/Person/Create.cshtml.cs
@@ -1,5 +1,7 @@
 using AdminPanel.EndPoint.ViewDto;
+using BLL.Dtos.Users;
 using BLL.ExternalApi;
+using BLL.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Reflection.PortableExecutable;
@@ -9,9 +11,11 @@ namespace AdminPanel.EndPoint.Pages.Users.Person
     public class CreateModel : PageModel
     {
         private readonly IFileUploadService uploadService;
-        public CreateModel(IFileUploadService uploadService)
+        private readonly IPersonService personService;
+        public CreateModel(IFileUploadService uploadService,IPersonService personService)
         {
             this.uploadService = uploadService;
+            this.personService = personService;
         }
         [BindProperty]
         public Add_PersonDto Person { get; set; }
@@ -21,11 +25,25 @@ namespace AdminPanel.EndPoint.Pages.Users.Person
         public async Task<IActionResult> OnPostAsync()
         {
             if (!ModelState.IsValid) return Page();
-
+            string avatarPath = "";
             if(Person.Avatar != null)
             {
-                uploadService.UploadAsync("Persons", Person.Avatar);
+                avatarPath = await uploadService.UploadAsync("Persons", Person.Avatar);
             }
+
+            PersonDto person = new PersonDto()
+            {
+                FirstName = Person.FirstName,
+                LastName = Person.LastName,
+                Email = Person.Email,
+                Password = Person.Password,
+                PhoneNumber = Person.PhoneNumber,
+                AvatarPath = avatarPath,
+            };
+            var result = await personService.AddPerson(person);
+            TempData["isSuccess"] = result.isSuccess;
+            TempData["message"] = result.Message;
+            return Page();
         }
     }
 }

e76629e6dc15eedaadfb595ce04ddecb984b2ad4
Farzad <amirifarzad1381@gmail.com>
2024-12-22 00:33:59 +0330

    find-person-by-email-service

--- DAL/Repositories/Person/PersonService.cs
+++ DAL/Repositories/Person/PersonService.cs
@@ -13,118 +13,144 @@ using DAL.Entities.Base;
 
 namespace DAL.Repositories.Person
 {
-	public class PersonService : IPersonService
-	{
-		private readonly string _connectionString;
-		public PersonService(string _connectionString)
-		{
-			this._connectionString = _connectionString;
-		}
-		public async Task<ResultDto<int?>> AddPerson(PersonDto person)
-		{
-			using var connection = new SqlConnection(_connectionString);
-			var existingUser = await connection.QuerySingleOrDefaultAsync<Personn>(
-				"GetPersonByEmail",
-				new { Email = person.Email },
-				commandType: CommandType.StoredProcedure
-			);
-			if (existingUser != null)
-			{
-				return new ResultDto<int?>()
-				{
-					isSuccess = false,
-					Message = "کاربری با این ایمیل موجود است"
-				};
-			}
-			await connection.CloseAsync();
+    public class PersonService : IPersonService
+    {
+        private readonly string _connectionString;
+        public PersonService(string _connectionString)
+        {
+            this._connectionString = _connectionString;
+        }
+        public async Task<ResultDto<int?>> AddPerson(PersonDto person)
+        {
+            using var connection = new SqlConnection(_connectionString);
+            var existingUser = await connection.QuerySingleOrDefaultAsync<Personn>(
+                "GetPersonByEmail",
+                new { Email = person.Email },
+                commandType: CommandType.StoredProcedure
+            );
+            if (existingUser != null)
+            {
+                return new ResultDto<int?>()
+                {
+                    isSuccess = false,
+                    Message = "کاربری با این ایمیل موجود است"
+                };
+            }
+            await connection.CloseAsync();
 
-			using var connection2 = new SqlConnection(_connectionString);
-			await connection2.OpenAsync();
-			using var transaction = connection2.BeginTransaction();
-			try
-			{ // Insert into Person table
+            using var connection2 = new SqlConnection(_connectionString);
+            await connection2.OpenAsync();
+            using var transaction = connection2.BeginTransaction();
+            try
+            { // Insert into Person table
 
-				var personId = await connection2.ExecuteScalarAsync<int>(
-					"InsertPerson",
-					new
-					{
-						FirstName = person.FirstName,
-						LastName = person.LastName,
-						PhoneNumber = person.PhoneNumber,
-						AvatarPath = person.AvatarPath,
-						Email = person.Email,
-						Password = person.Password
-					},
-					transaction,
-					commandType: CommandType.StoredProcedure
-				);
-				transaction.Commit();
-				if (personId != null)
-				{
-					return new ResultDto<int?>()
-					{
-						Data = personId,
-						isSuccess = true,
-						Message = $"کاربر با شناسه {personId} در سیستم ثبت شد"
-					};
-				}
-				else
-				{
-					return new ResultDto<int?>()
-					{
-						isSuccess = false,
-						Message = "خطا در ارتباط با پایگاه داده"
-					};
-				}
-			}
-			catch (Exception ex)
-			{
-				transaction.Rollback();
-				return new ResultDto<int?>()
-				{
-					isSuccess = false,
-					Message = $"خطایی رخ داد {ex.Message}"
-				};
-			}
-		}
+                var personId = await connection2.ExecuteScalarAsync<int>(
+                    "InsertPerson",
+                    new
+                    {
+                        FirstName = person.FirstName,
+                        LastName = person.LastName,
+                        PhoneNumber = person.PhoneNumber,
+                        AvatarPath = person.AvatarPath,
+                        Email = person.Email,
+                        Password = person.Password
+                    },
+                    transaction,
+                    commandType: CommandType.StoredProcedure
+                );
+                transaction.Commit();
+                if (personId != null)
+                {
+                    return new ResultDto<int?>()
+                    {
+                        Data = personId,
+                        isSuccess = true,
+                        Message = $"کاربر با شناسه {personId} در سیستم ثبت شد"
+                    };
+                }
+                else
+                {
+                    return new ResultDto<int?>()
+                    {
+                        isSuccess = false,
+                        Message = "خطا در ارتباط با پایگاه داده"
+                    };
+                }
+            }
+            catch (Exception ex)
+            {
+                transaction.Rollback();
+                return new ResultDto<int?>()
+                {
+                    isSuccess = false,
+                    Message = $"خطایی رخ داد {ex.Message}"
+                };
+            }
+        }
 
         public async Task<ResultDto> EditPerson(PersonDto Person)
         {
-			using(SqlConnection connection = new SqlConnection(_connectionString))
-			{
-				using (SqlCommand command = new SqlCommand("UpdatePerson", connection))
-				{
-					command.CommandType = CommandType.StoredProcedure;
-					command.Parameters.AddWithValue("@PersonID", Person.Id);
-					command.Parameters.AddWithValue("@FirstName", Person.FirstName);
-					command.Parameters.AddWithValue("@LastName", Person.LastName);
-					command.Parameters.AddWithValue("@Email", Person.Email);
-					command.Parameters.AddWithValue("@PhoneNumber", Person.PhoneNumber);
-					command.Parameters.AddWithValue("@Password", Person.Password);
-					command.Parameters.AddWithValue("@AvatarPath", Person.AvatarPath);
-					await connection.OpenAsync();
-					int rowsAffected = await command.ExecuteNonQueryAsync();
-					if (rowsAffected > 0)
-						return new ResultDto()
-						{
-							isSuccess = true,
-							Message = $"ویرایش کاربر با شناسه {Person.Id} با موفقیت انجام شد"
-						};
-					return new ResultDto()
-					{
-						isSuccess = false,
-						Message = "خطا در انجام ویرایش"
-					};
-				}
-			}
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                using (SqlCommand command = new SqlCommand("UpdatePerson", connection))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@PersonID", Person.Id);
+                    command.Parameters.AddWithValue("@FirstName", Person.FirstName);
+                    command.Parameters.AddWithValue("@LastName", Person.LastName);
+                    command.Parameters.AddWithValue("@Email", Person.Email);
+                    command.Parameters.AddWithValue("@PhoneNumber", Person.PhoneNumber);
+                    command.Parameters.AddWithValue("@Password", Person.Password);
+                    command.Parameters.AddWithValue("@AvatarPath", Person.AvatarPath);
+                    await connection.OpenAsync();
+                    int rowsAffected = await command.ExecuteNonQueryAsync();
+                    if (rowsAffected > 0)
+                        return new ResultDto()
+                        {
+                            isSuccess = true,
+                            Message = $"ویرایش کاربر با شناسه {Person.Id} با موفقیت انجام شد"
+                        };
+                    return new ResultDto()
+                    {
+                        isSuccess = false,
+                        Message = "خطا در انجام ویرایش"
+                    };
+                }
+            }
         }
 
         public async Task<ResultDto<PersonDto?>> GetPersonByEmail(string EmailAddress)
         {
-            using(SqlConnection connection = new SqlConnection(_connectionString))
-			{
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                var existPerson = await connection.QuerySingleOrDefaultAsync<Personn>(
+                    "GetPersonByEmail",
+                    new { Email = EmailAddress },
+                    commandType: CommandType.StoredProcedure);
+                if (existPerson != null)
+                    return new ResultDto<PersonDto?>()
+                    {
+                        isSuccess = true,
+                        Data = new PersonDto()
+                        {
+                            Id = existPerson.Id,
+                            FirstName = existPerson.FirstName,
+                            LastName = existPerson.LastName,
+                            PhoneNumber = existPerson.PhoneNumber,
+                            Email = existPerson.Email,
+                            Password = existPerson.Password,
+                            AvatarPath = existPerson.AvatarPath
+                        }
+                    };
 
-			}
+                else
+                    return new ResultDto<PersonDto?>()
+                    {
+                        isSuccess = false,
+                        Message = "کاربری با این ایمیل یافت نشد"
+                    };
+
+            }
         }
     }
 }

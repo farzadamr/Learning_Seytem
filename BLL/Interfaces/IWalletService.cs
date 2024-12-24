3c1a299b2b8b3ebd6a392a71c9c1d4f816512ae6
Farzad <amirifarzad1381@gmail.com>
2024-12-24 15:11:27 +0330

    wallet-interface

---
+++ BLL/Interfaces/IWalletService.cs
@@ -0,0 +1,18 @@
+﻿using BLL.Dtos.Utils;
+using BLL.Dtos.Wallet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Interfaces
+{
+    public interface IWalletService
+    {
+        Task<ResultDto<int?>> CreateWalletAsync(int StudentId);
+        Task<ResultDto<WalletDto?>> GetWalletAsync(int StudentId);
+        Task<ResultDto> ChargeWallet(int StudentId, int ChargeCount); 
+        Task<ResultDto> DeleteWalletAsync(int StudentId);
+    }
+}

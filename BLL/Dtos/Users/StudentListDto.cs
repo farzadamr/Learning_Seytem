3bed80304ad57e874c71e950a9f3a5e46ef8f3ca
Farzad <amirifarzad1381@gmail.com>
2024-12-25 21:32:05 +0330

    search-list-users-complete

--- BLL/Dtos/Users/StudentListDto.cs
+++ BLL/Dtos/Users/StudentListDto.cs
@@ -3,8 +3,8 @@
     public class StudentListDto
     {
         public int Id { get; set; }
-        public string Firstname { get; set; }
-        public string Lastname { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public string LinkedId { get; set; }
         public string ActivityArea { get; set; }
         public string Major { get; set; }
--- DAL/Repositories/SearchAndGet/SearchService.cs
+++ DAL/Repositories/SearchAndGet/SearchService.cs
@@ -51,14 +51,68 @@ namespace DAL.Repositories.SearchAndGet
             };
         }
 
-        Task<ResultDto<List<StudentListDto>?>> ISearchService.GetStudentByEmailSearch(string SearchKey)
+        public async Task<ResultDto<List<StudentListDto>?>> GetStudentByEmailSearch(string SearchKey)
         {
-            throw new NotImplementedException();
+            IEnumerable<StudentListDto> studentsEnum = new List<StudentListDto>();
+            if (string.IsNullOrWhiteSpace(SearchKey))
+            {
+                using (SqlConnection connection = new SqlConnection(_connectionString))
+                {
+                    studentsEnum = await connection.QueryAsync<StudentListDto>(
+                        "GetStudentList",
+                        commandType: CommandType.StoredProcedure);
+                }
+            }
+            else
+            {
+                using (SqlConnection connection2 = new SqlConnection(_connectionString))
+                {
+                    studentsEnum = await connection2.QueryAsync<StudentListDto>(
+                            "SearchStudentList",
+                            new { SearchKey = SearchKey },
+                            commandType: CommandType.StoredProcedure);
+                }
+            }
+            var students = studentsEnum.AsList();
+            if (students == null) return new ResultDto<List<StudentListDto>?> { isSuccess = false, Message = "خطا در ارتباط با پایگاه داده" };
+            return new ResultDto<List<StudentListDto>?>
+            {
+                Data = students,
+                isSuccess = true,
+                Message = "اطلاعات بازیابی شد"
+            };
         }
 
-        Task<ResultDto<List<TeacherListDto>?>> ISearchService.GetTeacherByEmailSearch(string SearchKey)
+        public async Task<ResultDto<List<TeacherListDto>?>> GetTeacherByEmailSearch(string SearchKey)
         {
-            throw new NotImplementedException();
+            IEnumerable<TeacherListDto> teachersEnum = new List<TeacherListDto>();
+            if (string.IsNullOrWhiteSpace(SearchKey))
+            {
+                using (SqlConnection connection = new SqlConnection(_connectionString))
+                {
+                    teachersEnum = await connection.QueryAsync<TeacherListDto>(
+                        "GetTeacherList",
+                        commandType: CommandType.StoredProcedure);
+                }
+            }
+            else
+            {
+                using (SqlConnection connection2 = new SqlConnection(_connectionString))
+                {
+                    teachersEnum = await connection2.QueryAsync<TeacherListDto>(
+                            "SearchTeacherList",
+                            new { SearchKey = SearchKey },
+                            commandType: CommandType.StoredProcedure);
+                }
+            }
+            var teachers = teachersEnum.AsList();
+            if (teachers == null) return new ResultDto<List<TeacherListDto>?> { isSuccess = false, Message = "خطا در ارتباط با پایگاه داده" };
+            return new ResultDto<List<TeacherListDto>?>
+            {
+                Data = teachers,
+                isSuccess = true,
+                Message = "اطلاعات بازیابی شد"
+            };
         }
     }
 }

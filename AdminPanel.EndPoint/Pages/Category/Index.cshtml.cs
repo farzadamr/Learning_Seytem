c172665881e8daf2fc624862cd56cc495a873259
Farzad <amirifarzad1381@gmail.com>
2024-12-25 18:06:51 +0330

    list-category-view

---
+++ AdminPanel.EndPoint/Pages/Category/Index.cshtml.cs
@@ -0,0 +1,48 @@
+using BLL.Dtos.Category;
+using BLL.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AdminPanel.EndPoint.Pages.Category
+{
+    public class IndexModel : PageModel
+    {
+        public List<CategoryDto> Categories { get; set; }
+        public List<CategoryDto> ChildCategories { get; set; }
+
+        private readonly ICategoryService _categoryService;
+        public IndexModel(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        public async Task OnGetAsync()
+        {
+            var findcategoryResult = await _categoryService.GetParentCategoryListAsync();
+            if (findcategoryResult.isSuccess)
+            {
+                Categories = findcategoryResult.Data;
+            }
+        }
+
+        public async Task<IActionResult> OnPostEditCategoryAsync(int editCategoryId, string editCategoryType)
+        {
+            var category = new CategoryDto { Id = editCategoryId, Type = editCategoryType };
+            var editResult = await _categoryService.EditCategoryAsync(category);
+            if (editResult.isSuccess)
+            {
+
+            }
+            return RedirectToPage();
+        }
+
+        public async Task<IActionResult> OnPostDeleteCategoryAsync(int deleteCategoryId)
+        {
+            await _categoryService.DeleteCategoryAsync(deleteCategoryId);
+            return RedirectToPage();
+        }
+    }
+}
+
